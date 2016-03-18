4aa918791d55a7aa0974b95082767313fd9ea8fe
Nicolette Furtado <ndfurtad@asu.edu>
2016-03-18 14:19:59 -0700

    Initial commit, code from BayesModel_user_step_repeated

---
+++ BayesModel_play_word/src/Constants.java
@@ -0,0 +1,27 @@
+public class Constants {
+	public static String CSV_FILE_SEPARATOR = ",";
+	public static String STUDENT_INPUT_DATA_SEPARATOR = " ";
+	public static String WORD_SEPARATOR = " ";
+	public static String MOVE_TO_HOTSPOT = "Move to Hotspot";
+	public static String MOVE_TO_OBJECT = "Move to Object";
+	public static String TAP = "Tap";
+	public static String CORRECT = "Correct";
+	public static String INCORRECT = "Incorrect";
+	public static String STORY_BEST_FARM = "Best Farm";
+	public static String EMPTY_STRING = "";
+	public static String DEFAULT_ACTION = "Default";
+	public static String KEY_SEPARATOR = "_";
+	public static String DEFAULT_WORD = "XYZ";
+	public static String DEFAULT_SENTENCE = "XYZ";
+
+	public static int CLEANING_UP_SENTENCES_TOTAL = 11;
+	public static double INITIAL_SKILL_VALUE = 0.35;
+
+	// files
+	public static String STUDENT_LOG_DATA = "C:\\Users\\Nicolette\\OneDrive\\Documents\\EMBRACE\\Analysis\\With user step\\log data by student\\log_data_B003BE_chapters_1_7.csv";
+	public static String OUTPUT_FILE = "C:\\Users\\Nicolette\\OneDrive\\Documents\\EMBRACE\\Analysis\\With user step\\output_B003BE_repeated_1.csv";
+	public static String INPUT_DATA = "inputdata/input_data_B003BE_the_best_farm"; // moves
+	public static String ACTION_FILE = "the_best_farm_actions.properties";
+	public static String WORDS_FILE = "the_best_farm_words.properties";
+
+}
