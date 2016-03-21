bd98100a48c8962a3aa88b61736d3ec4e2af59e6
Nicolette Furtado <ndfurtad@asu.edu>
2016-03-21 14:51:46 -0700

    Added new constants for play word and syntax conditions

--- BayesModel_play_word/src/Constants.java
+++ BayesModel_play_word/src/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 	public static String WORD_SEPARATOR = " ";
 	public static String MOVE_TO_HOTSPOT = "Move to Hotspot";
 	public static String MOVE_TO_OBJECT = "Move to Object";
+	public static String PLAY_WORD = "Play Word";
 	public static String TAP = "Tap";
 	public static String CORRECT = "Correct";
 	public static String INCORRECT = "Incorrect";
@@ -13,15 +14,18 @@ public class Constants {
 	public static String KEY_SEPARATOR = "_";
 	public static String DEFAULT_WORD = "XYZ";
 	public static String DEFAULT_SENTENCE = "XYZ";
+	public static String AUDIO_E = "E";
+	public static String AUDIO_FILE_ENDING = "E.m4a";
+	public static String SYNTAX = "Syntax";
 
 	public static int CLEANING_UP_SENTENCES_TOTAL = 11;
 	public static double INITIAL_SKILL_VALUE = 0.35;
 
 	// files
-	public static String STUDENT_LOG_DATA = "C:\\Users\\Nicolette\\OneDrive\\Documents\\EMBRACE\\Analysis\\With user step\\log data by student\\log_data_B003BE_chapters_1_7.csv";
-	public static String OUTPUT_FILE = "C:\\Users\\Nicolette\\OneDrive\\Documents\\EMBRACE\\Analysis\\With user step\\output_B003BE_repeated_1.csv";
-	public static String INPUT_DATA = "inputdata/input_data_B003BE_the_best_farm"; // moves
-	public static String ACTION_FILE = "the_best_farm_actions.properties";
-	public static String WORDS_FILE = "the_best_farm_words.properties";
+	public static String STUDENT_LOG_DATA = "C:\\Users\\Nicolette\\OneDrive\\Documents\\EMBRACE\\Analysis\\With user step\\log data with play word\\log_data_P0017EE_pw_cleaning_up.csv";
+	public static String OUTPUT_FILE = "C:\\Users\\Nicolette\\OneDrive\\Documents\\EMBRACE\\Analysis\\With user step\\output_P0017EE_pw.csv";
+	public static String INPUT_DATA = "inputdata/input_data_P0017EE_pw_cleaning_up"; // moves
+	public static String ACTION_FILE = "cleaning_up_actions.properties";
+	public static String WORDS_FILE = "cleaning_up_words.properties";
 
 }
