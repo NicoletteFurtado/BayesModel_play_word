51174d17b6244e9a0c71e3a66a07ce66a9073f77
Nicolette Furtado <ndfurtad@asu.edu>
2016-04-21 15:23:59 -0700

    Changes for play word (do not use)

--- BayesModel_play_word/src/KnowledgeTracer3.java
+++ BayesModel_play_word/src/KnowledgeTracer3.java
@@ -15,11 +15,6 @@ public class KnowledgeTracer3 {
 
 	public SkillSet calculateSkill(StudentLogData studentLogData, SkillSet skillSet, InitMaps initMaps) {
 		SkillSet skillSet1 = new SkillSet();
-		// double skillEvaluated = 0.0;
-		// double newSkill = 0.0;
-		// double prevSkillValue = 0.0;
-		// int prevUserStep = 0;
-		// String prevSentence = "";
 		skillMap = skillSet.getSkillMap();
 		// temporary variables
 		String currSentence = "";
@@ -28,8 +23,6 @@ public class KnowledgeTracer3 {
 		String currInputData = "";
 		String currVerification = "";
 
-		String word1 = "";
-		ArrayList<String> playWords = new ArrayList<String>();
 		// initialize wordToValueMap so that the last skill value of the word is stored
 		for (String word : skillMap.keySet()) {
 			wordToValueMap.put(word, skillMap.get(word).get(0));
@@ -53,12 +46,13 @@ public class KnowledgeTracer3 {
 		ArrayList<String> tempInputDataList = new ArrayList<String>();
 		ArrayList<String> tempVerificationList = new ArrayList<String>();
 
+		// System.out.println("sentence list=" + studentLogData.getSentenceList());
 		// ArrayList<String> userStepList = new ArrayList<String>();
 		ArrayList<String> wordList = new ArrayList<String>(); // for skills
 		for (int i = 0; i < studentLogData.getVerificationList().size(); i++) {
 			// get the current sentence and action
 			// System.out.println("i=" + i);
-			System.out.println(Arrays.toString(studentLogData.getSentenceList().toArray()));
+			// System.out.println(Arrays.toString(studentLogData.getSentenceList().toArray()));
 			currSentence = studentLogData.getSentenceList().get(i);
 			currAction = studentLogData.getActionList().get(i);
 			currUserStep = studentLogData.getUserStep().get(i);
@@ -73,6 +67,9 @@ public class KnowledgeTracer3 {
 			// System.out.println(tempSentenceList);
 			if (sentenceMap.keySet().contains(sentenceKey)) {
 				tempSentenceList = sentenceMap.get(sentenceKey);
+			} else {
+				tempSentenceList = new ArrayList<String>();
+				tempSentenceList.add(currSentence);
 			}
 			tempSentenceList.add(currSentence);
 			sentenceMap.put(sentenceKey, tempSentenceList);
@@ -81,6 +78,8 @@ public class KnowledgeTracer3 {
 			// tempActionList = sentenceMap.get(sentenceKey);
 			if (actionMap.keySet().contains(sentenceKey)) {
 				tempActionList = actionMap.get(sentenceKey);
+			} else {
+				tempActionList = new ArrayList<String>();
 			}
 			tempActionList.add(currAction);
 			actionMap.put(sentenceKey, tempActionList);
@@ -88,6 +87,8 @@ public class KnowledgeTracer3 {
 			// add inputdata
 			if (inputDataMap.keySet().contains(sentenceKey)) {
 				tempInputDataList = inputDataMap.get(sentenceKey);
+			} else {
+				tempInputDataList = new ArrayList<String>();
 			}
 			tempInputDataList.add(currInputData);
 			inputDataMap.put(sentenceKey, tempInputDataList);
@@ -95,6 +96,8 @@ public class KnowledgeTracer3 {
 			// add userStep
 			if (userStepMap.keySet().contains(sentenceKey)) {
 				tempUserStepList = userStepMap.get(sentenceKey);
+			} else {
+				tempUserStepList = new ArrayList<Integer>();
 			}
 			tempUserStepList.add(currUserStep);
 			userStepMap.put(sentenceKey, tempUserStepList);
@@ -102,6 +105,8 @@ public class KnowledgeTracer3 {
 			// add verification
 			if (verificationMap.keySet().contains(sentenceKey)) {
 				tempVerificationList = verificationMap.get(sentenceKey);
+			} else {
+				tempVerificationList = new ArrayList<String>();
 			}
 			tempVerificationList.add(currVerification);
 			verificationMap.put(sentenceKey, tempVerificationList);
@@ -116,6 +121,7 @@ public class KnowledgeTracer3 {
 		// evaluateSentence(actionMap, sentenceMap, userStepMap, inputDataMap, verificationMap, initMaps,
 		// studentLogData);
 		// }
+
 		evaluateSentence(actionMap, sentenceMap, userStepMap, inputDataMap, verificationMap, initMaps, studentLogData);
 		skillSet1.setSkillMap(skillMap);
 		return skillSet1;
@@ -127,27 +133,40 @@ public class KnowledgeTracer3 {
 			LinkedHashMap<String, ArrayList<String>> inputDataMap,
 			LinkedHashMap<String, ArrayList<String>> verificationMap, InitMaps initMaps, StudentLogData student) {
 
-		ArrayList<String> tempSentenceList;
-		ArrayList<String> tempActionList;
-		ArrayList<Integer> tempUserStepList;
-		ArrayList<String> tempInputDataList;
-		ArrayList<String> tempVerificationList;
+		// ArrayList<String> tempSentenceList;
+		// ArrayList<String> tempActionList;
+		// ArrayList<Integer> tempUserStepList;
+		// ArrayList<String> tempInputDataList;
+		// ArrayList<String> tempVerificationList;
 
 		// for each sentence
 		for (String sentence : actionMap.keySet()) {
 			// for each step update each skill
 			// get local arrayLists
 			System.err.println("/////////// " + sentence);
-			tempActionList = actionMap.get(sentence);
-			System.out.println("/////////1 " + Arrays.toString(tempActionList.toArray()));
-			tempUserStepList = userStepMap.get(sentence);
-			tempInputDataList = inputDataMap.get(sentence);
-			tempVerificationList = verificationMap.get(sentence);
-			calculateForSentence(sentence, tempActionList, tempUserStepList, tempInputDataList, tempVerificationList,
-					initMaps, student);
+			// tempActionList = actionMap.get(sentence);
+			// System.out.println("/////////1 " + Arrays.toString(tempActionList.toArray()));
+			// tempUserStepList = userStepMap.get(sentence);
+			// tempInputDataList = inputDataMap.get(sentence);
+			// tempVerificationList = verificationMap.get(sentence);
+			// calculateForSentence(sentence, tempActionList, tempUserStepList, tempInputDataList, tempVerificationList,
+			// initMaps, student);
+			calculateForSentence(sentence, actionMap.get(sentence), userStepMap.get(sentence),
+					inputDataMap.get(sentence), verificationMap.get(sentence), initMaps, student);
 			// last step update syntax
 			// see number of play words
 			// update the play words differently than others
+		}
+	}
+
+	private void evaluateSentence2(LinkedHashMap<String, ArrayList<String>> actionMap,
+			LinkedHashMap<String, ArrayList<String>> sentenceMap,
+			LinkedHashMap<String, ArrayList<Integer>> userStepMap,
+			LinkedHashMap<String, ArrayList<String>> inputDataMap,
+			LinkedHashMap<String, ArrayList<String>> verificationMap, InitMaps initMaps, StudentLogData student) {
+		System.out.println("action map=" + actionMap);
+		for (String sentence : actionMap.keySet()) {
+
 		}
 	}
 
@@ -158,55 +177,70 @@ public class KnowledgeTracer3 {
 		double skillEvaluated = 0.0;
 		double newSkill = 0.0;
 		double prevSkillValue = 0.0;
+		int prevUserStep = 0;
+		String prevAction = "";
+		// int currUserStep = 0;
 
 		ArrayList<String> playWordList = new ArrayList<String>();
 		for (int i = 0; i < tempActionList.size(); i++) {
 			HashMap<String, Boolean> wordToVerif = checkIncorrectSkill2(studentLogData, initMaps, i);
-			if (tempVerificationList.get(i).equals(Constants.CORRECT)) {
-				for (String word : wordToVerif.keySet()) {
-					// System.err.println("wordToVerif" + wordToVerif);
-					prevSkillValue = wordToValueMap.get(word).getSkillValue();
-					// change skills differently if they are in playWords list
-					if (playWordList.equals(word))
-						skillEvaluated = this.calcCorrectPlayWord(studentLogData, prevSkillValue);
-					else
-						skillEvaluated = this.calcCorrect(studentLogData, prevSkillValue);
-					newSkill = calcNewSkillValue(studentLogData, skillEvaluated);
-					updateSkills(studentLogData, newSkill, word, tempVerificationList.get(i), sentence,
-							tempActionList.get(i), tempUserStepList.get(i).intValue());
-				}
-				System.out.println(wordToVerif);
-
-			} else if (tempVerificationList.get(i).equals(Constants.INCORRECT)) {
-				for (String word : wordToVerif.keySet()) {
-					if (wordToValueMap.keySet().contains(word)) { // some words that the student moved to are
-																	// not skills
-						if (wordToVerif.get(word)) {
+			// if currAction!=play word && prevuserstep!=currUserStep &&
+			if (!tempActionList.get(i).equals(Constants.PLAY_WORD)
+					&& (!(prevUserStep == studentLogData.getUserStep().get(i).intValue()) || prevAction
+							.equals(Constants.PLAY_WORD))) {
+				if (tempVerificationList.get(i).equals(Constants.CORRECT)) {
+					for (String word : wordToVerif.keySet()) {
+						if (wordToValueMap.keySet().contains(word)) { // some words that the student moved to are
+							// not skills
+							// System.err.println("wordToVerif" + wordToVerif);
 							prevSkillValue = wordToValueMap.get(word).getSkillValue();
-							// skillEvaluated = this.calcCorrect(studentLogData, prevSkillValue);
+							// change skills differently if they are in playWords list
 							if (playWordList.equals(word))
 								skillEvaluated = this.calcCorrectPlayWord(studentLogData, prevSkillValue);
 							else
 								skillEvaluated = this.calcCorrect(studentLogData, prevSkillValue);
-							// System.out.println("in incorrect correct " + word);
-						} else {
-							prevSkillValue = wordToValueMap.get(word).getSkillValue();
-							if (playWordList.equals(word))
-								skillEvaluated = this.calcIncorrectPlayWord(studentLogData, prevSkillValue);
-							else
-								skillEvaluated = this.calcIncorrect(studentLogData, prevSkillValue);
-							// System.out.println("in incorrect incorrect " + word);
+							newSkill = calcNewSkillValue(studentLogData, skillEvaluated);
+							updateSkills(studentLogData, newSkill, word, tempVerificationList.get(i), sentence,
+									tempActionList.get(i), tempUserStepList.get(i).intValue());
 						}
-						newSkill = calcNewSkillValue(studentLogData, skillEvaluated);
-						// updateSkills(studentLogData, initMaps, newSkill, word, i);
-						updateSkills(studentLogData, newSkill, word, tempVerificationList.get(i), sentence,
-								tempActionList.get(i), tempUserStepList.get(i).intValue());
-						System.out.println("updated incorrect");
+					}
+					// System.out.println(wordToVerif);
+
+				} else if (tempVerificationList.get(i).equals(Constants.INCORRECT)) {
+					for (String word : wordToVerif.keySet()) {
+						if (wordToValueMap.keySet().contains(word)) { // some words that the student moved to are
+																		// not skills
+							if (wordToVerif.get(word)) {
+								prevSkillValue = wordToValueMap.get(word).getSkillValue();
+								// skillEvaluated = this.calcCorrect(studentLogData, prevSkillValue);
+								if (playWordList.equals(word))
+									skillEvaluated = this.calcCorrectPlayWord(studentLogData, prevSkillValue);
+								else
+									skillEvaluated = this.calcCorrect(studentLogData, prevSkillValue);
+								// System.out.println("in incorrect correct " + word);
+							} else {
+								prevSkillValue = wordToValueMap.get(word).getSkillValue();
+								if (playWordList.equals(word))
+									skillEvaluated = this.calcIncorrectPlayWord(studentLogData, prevSkillValue);
+								else
+									skillEvaluated = this.calcIncorrect(studentLogData, prevSkillValue);
+								// System.out.println("in incorrect incorrect " + word);
+							}
+							newSkill = calcNewSkillValue(studentLogData, skillEvaluated);
+							// updateSkills(studentLogData, initMaps, newSkill, word, i);
+							updateSkills(studentLogData, newSkill, word, tempVerificationList.get(i), sentence,
+									tempActionList.get(i), tempUserStepList.get(i).intValue());
+							// System.out.println("updated incorrect");
+						}
 					}
 				}
+				// prevUserStep = studentLogData.getUserStep().get(i).intValue();
+				// prevAction = studentLogData.getActionList().get(i);
 			} else if (tempActionList.get(i).equals(Constants.PLAY_WORD)) {
 				playWordList.add(tempInputDataList.get(i));
 			}
+			prevUserStep = studentLogData.getUserStep().get(i).intValue();
+			prevAction = studentLogData.getActionList().get(i);
 		}
 	}
 
@@ -259,8 +293,8 @@ public class KnowledgeTracer3 {
 		skillMap.get(word).add(skill);
 		//
 		wordToValueMap.put(word, skill);
-		System.out.println("changed value " + word + " " + newSkill + " for sentence " + skill.getSentence()
-				+ " for step " + userStep);
+		// System.out.println("changed value " + word + " " + newSkill + " for sentence " + skill.getSentence()
+		// + " for step " + userStep);
 	}
 
 	// set false for the word that the student got wrong
@@ -283,11 +317,12 @@ public class KnowledgeTracer3 {
 		wordToVerif.put(Constants.SYNTAX, false);
 		// case for pen
 		for (int i = 0; i < objectsMoved.length; i++) {
-			if (objectsMoved[i].contains("pen2")) {
+			if (objectsMoved[i].contains("pen1") || objectsMoved[i].contains("pen2")
+					|| objectsMoved[i].contains("pen3") || objectsMoved[i].contains("pen4")) {
 				objectsMoved[i] = "pen";
 			} else if (objectsMoved[i].contains("corralDoor")) {
 				objectsMoved[i] = "corral";
-			} else if (objectsMoved[i].contains("pumpkinPatch")) {
+			} else if (objectsMoved[i].contains("pumpkinPatch") || objectsMoved[i].contains("pumpkin")) {
 				objectsMoved[i] = "pumpkins";
 			} else if (objectsMoved[i].contains("farmerFall")) {
 				objectsMoved[i] = "farmer";
@@ -303,23 +338,28 @@ public class KnowledgeTracer3 {
 				// wordToVerif.put(Constants.SYNTAX, true);
 			} else {
 				wordToVerif.put(actionWords.get(i), false);
-				if (actionWords.contains(objectsMoved[i])) {
-					// if the students moved the correct words in the wrong order
-					// wordToVerif.put(Constants.SYNTAX, true);
-				}
+				// if (actionWords.contains(objectsMoved[i])) {
+				// if the students moved the correct words in the wrong order
+				// wordToVerif.put(Constants.SYNTAX, true);
+				// }
 			}
 		}
 		// check if all words in actionWords are present in objectsMoved
-		boolean flag = false;
+		int flag = 99;
 		for (int i = 0; i < actionWords.size(); i++) {
 			if (actionWords.contains(objectsMoved[i])) {
-				flag = true;
+				flag = 1;
 			} else {
-				flag = false;
+				flag = 0;
 				break;
 			}
 		}
-		wordToVerif.put(Constants.SYNTAX, flag);
+		if (flag == 1)
+			wordToVerif.put(Constants.SYNTAX, true);
+		else if (flag == 0)
+			wordToVerif.put(Constants.SYNTAX, false);
+		else
+			System.err.println("flag=" + flag);
 		return wordToVerif;
 	}
 }
